287a912d80c2d88013afe98a1542668b18397683
Your Name <you@example.com>
2026-02-25 18:30:27 +0500

    erfve

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    Question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    Question_text = models.ForeignKey(Question, on_delete=models.CASCADE)
+    Choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+from django.db import models
+
+
+class Question(models.Model):
+    # ... 
+    def __str__(self):
+        return self.Question_text
+
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):
+        return self.Choice_text
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
