20ae4cd159845c4a8a1ebc8d952716662c7dbc5e
Your Name <you@example.com>
2026-03-02 18:16:41 +0500

    sjgksjr

--- polls/apps.py
+++ polls/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    name = 'polls'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_polls"
+    label = "polls"
+
--- polls/models.py
+++ polls/models.py
@@ -38,3 +38,24 @@ class Question(models.Model):
     # ...
     def __str__(self):
         return self.Question_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+from django.contrib import admin
+
+
+class Question(models.Model):
+    # ...
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
